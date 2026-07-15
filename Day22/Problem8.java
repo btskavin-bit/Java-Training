98b7763b17e917add824572a2502236fcf1d3a17
kavin <btskavin@gmail.com>
2026-07-15 09:43:37 +0530

    Day22 mod

--- Day22/Problem8.java
+++ Day22/Problem8.java
@@ -1,5 +1,26 @@
-package Day22;
-
+import java.util.HashMap;
+import java.util.Map;
 public class Problem8 {
-    
+    public static boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            int remainder = sum % k;
+            if (map.containsKey(remainder)) {
+                if (i - map.get(remainder) >= 2) {
+                    return true;
+                }
+            } else {
+                map.put(remainder, i);
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] nums = {23, 2, 4, 6, 7};
+        int k = 6;
+        System.out.println(checkSubarraySum(nums, k));
+    }
 }
--- Day22/Problem9.java
+++ Day22/Problem9.java
@@ -1,5 +1,30 @@
-package Day22;
-
+import java.util.HashMap;
+import java.util.Map;
 public class Problem9 {
-    
+    public static int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum = 0;
+        int maxLength = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                sum -= 1;
+            } else {
+                sum += 1;
+            }
+            if (map.containsKey(sum)) {
+                int length = i - map.get(sum);
+                if (length > maxLength) {
+                    maxLength = length;
+                }
+            } else {
+                map.put(sum, i);
+            }
+        }
+        return maxLength;
+    }
+    public static void main(String[] args) {
+        int[] nums = {0, 1};
+        System.out.println(findMaxLength(nums));
+    }
 }
